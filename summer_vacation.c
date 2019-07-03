128d1cfdf218bf4b4017d880ecf4b2d8bd848e40
seongminyoo <dbtjdals1771@ajou.ac.kr>
2019-07-03 22:42:47 +0900

    update

--- summer_vacation.c
+++ summer_vacation.c
@@ -346,3 +346,56 @@ int factorial(int n){
 }
 */
 
+#include <stdio.h>
+
+char p[6562][6562];
+void Star(int n,int x,int y);
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j==n) p[i][j] = '\0';
+            else p[i][j] = ' ';
+        }
+    }
+    
+    Star(n,(n-1)/2,(n-1)/2);
+
+    for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%c", p[i][j]);
+		}
+		printf("\n");
+	}
+	
+    return 0;
+}
+
+void Star(int n,int x, int y){
+    if(n==3){
+        p[y-1][x-1] = '*';
+        p[y-1][x] = '*';
+        p[y-1][x+1] = '*';
+        p[y][x-1] = '*';
+        p[y][x+1] = '*';
+        p[y+1][x-1] = '*';
+        p[y+1][x] = '*';
+        p[y+1][x+1] = '*';
+        return;
+    }
+
+    Star(n/3,x-(n/3),y-(n/3)); // 1
+    Star(n/3,x,y-(n/3));
+    Star(n/3,x+(n/3),y-(n/3));
+    Star(n/3,x-(n/3),y); // 4
+    Star(n/3,x+(n/3),y);
+    Star(n/3,x-(n/3),y+(n/3)); // 7
+    Star(n/3,x,y+(n/3));
+    Star(n/3,x+(n/3),y+(n/3));
+}
+
